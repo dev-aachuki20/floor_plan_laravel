a974c3d9a1ff89384e2be44a64cf26fec615bf26
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2024-07-13 15:21:58 +0530

    make user uuid unique

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
 
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid');
+            $table->uuid('uuid')->unique();
 
             $table->unsignedBigInteger('primary_role');
             $table->unsignedBigInteger('hospital')->nullable();
