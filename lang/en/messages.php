f3c7c78c13ff6e3889d5fd0f0a9ed9869ae28390
Aachukihipl <aachukiagarwal.hipl@gmail.com>
2024-08-10 10:27:12 +0530

    message

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -2,45 +2,45 @@
 
 return [
 
-    'crud'=>[
+    'crud' => [
         'add_record'    => 'Successfully Added !',
         'update_record' => 'Successfully Updated !',
         'delete_record' => 'This record has been succesfully deleted!',
-        'restore_record'=> 'This record has been succesfully Restored!',
+        'restore_record' => 'This record has been succesfully Restored!',
         'merge_record'  => 'This record has been succesfully Merged!',
-        'approve_record'=> 'Record Successfully Approved !',
+        'approve_record' => 'Record Successfully Approved !',
         'status_update' => 'Status successfully updated!',
     ],
 
     'unable_to_add_blank_field' => 'Sorry, Unable to add a blank field in',
     'data_already_exists' => 'Sorry, You cannot create new with the same name so use existing.',
 
-    'areYouSure'=>'Are you sure you want to delete this record?',
-    'areYouSureapprove'=>'Are you sure you want to Approve this record?',
-    'areYouSurerestore'=>'Are you sure you want to Restore this Database? It will delete your current database.',
-    'deletetitle'=>'Delete Confirmation',
-    'restoretitle'=>'Restore Confirmation',
-    'approvaltitle'=>'Approval Confirmation',
-    'areYouSureRestore'=>'Are you sure you want to restore this record?',
+    'areYouSure' => 'Are you sure you want to delete this record?',
+    'areYouSureapprove' => 'Are you sure you want to Approve this record?',
+    'areYouSurerestore' => 'Are you sure you want to Restore this Database? It will delete your current database.',
+    'deletetitle' => 'Delete Confirmation',
+    'restoretitle' => 'Restore Confirmation',
+    'approvaltitle' => 'Approval Confirmation',
+    'areYouSureRestore' => 'Are you sure you want to restore this record?',
     'error_message'   => 'Something went wrong....please try again later!',
     'no_record_found' => 'No Records Found!',
-    'suspened'=> "Your account has been suspened!",
-    'invalid_email'=>'Invalid Email',
-    'invalid_otp'=>'Invalid OTP',
-    'invalid_pin'=>'Invalid PIN',
-    'wrong_credentials'=>'These credentials do not match our records!',
-    'not_activate'=>'Your account is not activated.',
-    'otp_sent_email'=>'We have successfully sent OTP on your Registered Email',
-    'expire_otp'=> 'OTP has been Expired',
-    'verified_otp'=> 'OTP successfully Verified.',
-    'invalid_token_email'=> 'Invalid Token or Email!',
-    'success'=>'Success',
-    'register_success'=>'Registration successful! Please check your email for a verification link.',
-    'login_success'=>'You have logged in successfully!',
-    'logout_success'=>'Logged out successfully!',
-    'warning_select_record'=> 'Please select at least one record',
-    'required_role'=> "User with the specified email doesn't have the required role.",
-    
+    'suspened' => "Your account has been suspened!",
+    'invalid_email' => 'Invalid Email',
+    'invalid_otp' => 'Invalid OTP',
+    'invalid_pin' => 'Invalid PIN',
+    'wrong_credentials' => 'These credentials do not match our records!',
+    'not_activate' => 'Your account is not activated.',
+    'otp_sent_email' => 'We have successfully sent OTP on your Registered Email',
+    'expire_otp' => 'OTP has been Expired',
+    'verified_otp' => 'OTP successfully Verified.',
+    'invalid_token_email' => 'Invalid Token or Email!',
+    'success' => 'Success',
+    'register_success' => 'Registration successful! Please check your email for a verification link.',
+    'login_success' => 'You have logged in successfully!',
+    'logout_success' => 'Logged out successfully!',
+    'warning_select_record' => 'Please select at least one record',
+    'required_role' => "User with the specified email doesn't have the required role.",
+
     'invalid_token'                 => 'Your access token has been expired. Please login again.',
     'not_authorized'                => 'Not Authorized to access this resource/api',
     'not_found'                     => 'Not Found!',
@@ -48,7 +48,7 @@ return [
     'resource_not_found'            => 'Resource not found',
     'token_invalid'                 => 'Token is invalid',
     'unexpected'                    => 'Unexpected Exception. Try later',
-    
+
     'data_retrieved_successfully'   => 'Data retrieved successfully',
     'record_retrieved_successfully' => 'Record retrieved successfully',
     'record_created_successfully'   => 'Record created successfully',
@@ -70,7 +70,7 @@ return [
     'user_created_and_welcome_email_sent'  => 'User created successfully and a welcome email has been sent.',
     'user_updated_and_email_sent'  => 'User updated successfully and email has been sent.',
 
-    'reports'=>[
+    'reports' => [
         'speciality' => [
             'title'       => 'Surgeons',
             'description' => 'The following graphic shows the level of progress of prepared surgeons over the week in the selected hospital.'
@@ -82,7 +82,10 @@ return [
         'staff' => [
             'title'       => 'Staff',
             'description' => 'The following graphic shows the level of progress of prepared staff over the week in the selected hospital.'
+        ],
+        'overview' => [
+            'title' => 'Overview',
+            'descriptiion' => 'The following graphic shows the level of progress in approved surgical procedures throughout the week in the selected hospital.',
         ]
     ]
-    
 ];
