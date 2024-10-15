2637628964766d3281e03f94548b9df24f5d42fd
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2024-10-15 14:44:49 +0530

    fixing the bug

--- resources/views/emails/auth/welcome.blade.php
+++ resources/views/emails/auth/welcome.blade.php
@@ -11,13 +11,13 @@
         <li style="color: #000;"><strong>Role:</strong> {{ $user->role->role_name ?? '' }}</li>
     </ul>
 
-    @if($mfaMethod == 'google')
+    @if(getSetting('mfa_method') == 'google')
      
        @if($qrCodeImageUrl)
 
        <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">To access the system you first need to activate your security MFA token by scanning the following QR code using your Google authenticator app</p>
 
-       <div style="text-align: center;"><img src="{{ $qrCodeImageUrl }}" style="max-width: 250px;width: 100%;"/></div>
+       <div style="text-align: center;"><img src="{{ $qrCodeImageUrl ?? '' }}" style="max-width: 250px;width: 100%;"/></div>
 
        @else
 
@@ -25,18 +25,18 @@
 
        @endif
 
-    @elseif($mfaMethod == 'email')
+    @elseif(getSetting('mfa_method') == 'email')
         <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">To proceed with logging in, please verify your identity using the OTP provided below:</p>
 
-        <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">Never share OTP with anyone: <br><span style="background-color: #295597;color: #fff;padding: 7px 15px;display: table;margin: 10px auto 0;border-radius: 4px;font-weight: bold;">{{ $otp }}</span></p>
+        <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">Never share OTP with anyone: <br><span style="background-color: #295597;color: #fff;padding: 7px 15px;display: table;margin: 10px auto 0;border-radius: 4px;font-weight: bold;">{{ $otp ?? '' }}</span></p>
 
-        <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">(This OTP will expire in {{ $otp_expiry }} minutes.)</p>
+        <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">(This OTP will expire in {{ $otp_expiry ?? '' }} minutes.)</p>
 
     @endif
 
     <a href="{{ config('app.site_url') }}/login" style="font-family: 'Barlow', sans-serif; color:#fff; text-transform: uppercase; font-size:18px; line-height: 13px; border-radius: 5px; background-color: #006AF2;box-shadow:8px 6px 15px 0px rgba(0, 97, 222, 0.25); padding: 21px 28px; display: inline-block; text-decoration: none;margin-bottom: 27px;width: 100%;text-align: center;">Login</a>
 
-    <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">Click the link to "Set Your Password" or Copy and Paste below URL into your web browser: <a href="{{ $setPasswordUrl }}" style="word-break: break-all;">{{ $setPasswordUrl }}</a></p>
+    <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">Click the link to "Set Your Password" or Copy and Paste below URL into your web browser: <a href="{{ $setPasswordUrl ?? '#' }}" style="word-break: break-all;">{{ $setPasswordUrl ?? '' }}</a></p>
 
     <p style="font-size: 16px; line-height: 25.5px; font-weight: normal; font-family: 'Nunito Sans', sans-serif; color: #000; margin-bottom: 27px;margin-top: 10px;">If you have any questions, feel free to reach out to us.</p>
 
